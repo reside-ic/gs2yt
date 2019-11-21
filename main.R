a5eb87a1b38365bf96446b4ed44332cacc646a4b
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-11-21 14:33:29 +0000

    Add entrypoint

---
+++ main.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+local({
+  env <- new.env(parent = parent.env(topenv()))
+  for (p in dir("R", full.names = TRUE)) {
+    sys.source(p, env)
+  }
+  attach(env, name = "gs2yt")
+})
+
+main()
